18177f7f6b815d8ee6a65fa80a887ec5cacba80d
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-10-20 15:05:53 +0900

    refactored.

--- src/smb1.c
+++ src/smb1.c
@@ -3,6 +3,31 @@
 #include <stdlib.h>
 #include <msx.h>
 
+#define max(a, b)   ((a) >= (b) ? (a) : (b))
+
+/**
+ * Type of 16 bit signed Q6 fixed point number.
+ * - integer part : 10 bits
+ * - decimal part : 6 bits
+ */
+typedef int16_t f10q6_t;
+
+/**
+ * Constructs a f10q6_t value from integer.
+ * \param i   an integer, that shall be 10 bits signed integer.
+ */
+#define f10q6i(i)    ((f10q6_t)((i) << 6))
+
+/**
+ * Type of 22 bit signed fixed point number.
+ * - integer part : 16 bits
+ * - decimal part : 6 bits
+ */
+typedef struct f16q6_t {
+  int16_t i;                    /* integer part */
+  f10q6_t d;                    /* decimal part */
+} f16q6_t;
+
 /* defined sprite pattern table */
 extern const size_t smb1spt_size;
 extern const char smb1spt[];
@@ -18,44 +43,6 @@ extern const char smb1tile[];
 /* stage map */
 extern const size_t smb1map_size;
 extern const char smb1map[];
-
-/* #define SCREEN_MODE     VDP_SCREEN_MODE_GRAPHIC_1 */
-/* #define SCREEN_LINES    VDP_SCREEN_LINES_192 */
-/* #define PATTERNS        (0x00000) */
-/* #define IMAGE           (0x01800) */
-/* #define SPRITES         (0x01B00) */
-/* #define COLORS          (0x02000) */
-/* #define SPRITE_PATTERNS (0x03800) */
-
-/* #define SIZE_OF_IMAGE    (0x0300) */
-/* #define SIZE_OF_PATTERNS (0x0800) */
-/* #define SIZE_OF_COLORS  (32) */
-
-/* #define SCREEN_MODE     VDP_SCREEN_MODE_GRAPHIC_3 */
-/* #define SCREEN_LINES    VDP_SCREEN_LINES_192 */
-/* #define PATTERNS        (0x00000) */
-/* #define IMAGE           (0x01800) */
-/* #define SPRITES         (0x01E00) */
-/* #define COLORS          (0x02000) */
-/* #define SPRITE_PATTERNS (0x03800) */
-/* #define SPRITE_COLORS   (SPRITES - 0x0200) */
-
-/* #define SIZE_OF_IMAGE    (0x0300) */
-/* #define SIZE_OF_PATTERNS (0x1800) */
-/* #define SIZE_OF_COLORS   (0x1800) */
-
-/* #define SCREEN_MODE     VDP_SCREEN_MODE_GRAPHIC_3 */
-/* #define SCREEN_LINES    VDP_SCREEN_LINES_212 */
-/* #define PATTERNS        (0x00000) */
-/* #define COLORS          (0x02000) */
-/* #define IMAGE           (0x04000) */
-/* #define SPRITES         (0x04600) */
-/* #define SPRITE_PATTERNS (0x04800) */
-/* #define SPRITE_COLORS   (SPRITES - 0x0200) */
-
-/* #define SIZE_OF_IMAGE    (0x0400) */
-/* #define SIZE_OF_PATTERNS (0x2000) */
-/* #define SIZE_OF_COLORS   (0x2000) */
 
 #define SCREEN_MODE             VDP_SCREEN_MODE_GRAPHIC_4
 #define SCREEN_LINES            VDP_SCREEN_LINES_212
@@ -85,6 +72,8 @@ extern const char smb1map[];
 #define STAGEMAP_PAGE_ROWS      (16)
 #define STAGEMAP_VISIBLE_COLS   (16)
 #define STAGEMAP_VISIBLE_ROWS   (14)
+
+#define SCROLL_SPEED_MAX (f10q6i(32))
 
 static const palette_t color_palette[16] = {
   RGB(0,0,0), RGB(6,1,0), RGB(3,3,1), RGB(7,5,4),
@@ -169,80 +158,145 @@ static void setup_stage_map(void) {
 }
 
 static void show_sprites(void) {
-  /* Show 16 sprite characters for demo */
+  // Show 16 sprite characters for demo
   struct sprite s = {0};
   for (int i = 0; i < 32; ++i) {
-    int x =  96 + 16 * (i / 8) + 4;
-    int y = 127 + 16 * (i / 2 % 4);
+    int x =  8 + 16 * (i / 2 % 4);
+    int y = -1 + 16 * (i / 8);
     sprite_set_xy(&s, x, y);
     s.pat = 4 * i;
-    vmem_write(SPRITES + 4 * i, &s, 4);
+    vmem_write(SPRITES + 4 * i, &s, sizeof(struct sprite));
   }
+  // ---- Mario (player) ---- (#0,#1)
+  sprite_set_xy(&s, 124, 175);
+  s.pat = 0;
+  vmem_write(SPRITES + 0, &s, sizeof(struct sprite));
+  s.pat = 4;
+  vmem_write(SPRITES + 4, &s, sizeof(struct sprite));
 }
 
 /* timer tick counter */
 static uint16_t tick;
 
 /* number of columns of stage map */
-static uint16_t WD;
+static uint16_t map_cols;
 
 /* next map column to be written */
-static uint16_t WX;
+static uint16_t map_next;
 
 /* map position (camera position) */
-static uint16_t SX;             /* integer part of camera position */
-static uint16_t SXd;            /* decimal part of camera position */
-static uint16_t scroll_speed = 0; /* scroll speed ; 10.6 fixedpoint [pix] */
-static const uint16_t SCROLL_SPEED_MAX = (10 << 6); /* 10.6 fixedpoint [pix] */
+static struct {
+  f16q6_t x;
+  f10q6_t speed;
+} camera;
 
 /* accumulator for cooperative task interleaving */
 static uint8_t timeslots_counter;
 
-// Time sharing scheduling ; core task : renderer = 16:10
-// Because TILE_WIDTH = 16 and SCROLL_SPEED_MAX = 10
-static const uint8_t TIMESLOTS_FOR_CORE_TASK = 16; /* regular task */
-static const uint8_t TIMESLOTS_FOR_RENDERER  = 10; /* interleaved task */
+struct mario_state {
+  uint8_t input;
+  f10q6_t speed;
+  enum facing {
+    FACING_LEFT  = 0,
+    FACING_RIGHT = 1,
+  } facing;
+};
+
+static struct mario_state  mario_state = {
+  .input = 0,
+  .speed = 0,
+  .facing = FACING_RIGHT,
+};
+
+enum mario_pose {
+  STANDING    = 0,
+  STANDING_L  = 0,
+  STANDING_R  = 1,
+
+  JUMPING     = 2,
+  JUMPING_L   = 2,
+  JUMPING_R   = 3,
+
+  WALKING_1   = 4,
+  WALKING_1_L = 4,
+  WALKING_1_R = 5,
+
+  WALKING_2   = 6,
+  WALKING_2_L = 6,
+  WALKING_2_R = 7,
+
+  WALKING_3   = 8,
+  WALKING_3_L = 8,
+  WALKING_3_R = 9,
+
+  DEAD        = 10,
+};
 
 static void animation_task(void) {
   uint8_t j;
-  /* sprite animation #1 */
-  {
-    j = tick % 3;
-    vmem_write(SPRITE_PATTERNS+0x000, smb1spt + 64 * (2 * j + 5), 64);
+
+  if (mario_state.speed == 0) {
+    vmem_write(SPRITE_PATTERNS+0x000,
+               smb1spt + 64 * (STANDING + mario_state.facing),
+               64);
+  } else {
+    const uint8_t b = (mario_state.input & VK_FIRE_1) ? 1 : 2;
+    if (!(tick & ((1 << b) - 1))) {
+      j = (tick >> b) % 3;
+      vmem_write(SPRITE_PATTERNS+0x000,
+                 smb1spt + 64 * (2*j + (WALKING_1 + mario_state.facing)),
+                 64);
+    }
   }
-  /* sprite animation #2 */
-  if (!(tick & 1)) {
-    j = (tick >> 1) % 3;
-    vmem_write(SPRITE_PATTERNS+0x100, smb1spt + 64 * (2 * j + 5), 64);
-  }
-  /* sprite animation #3 */
-  if (!(tick & 3)) {
-    j = (tick >> 2) % 3;
-    vmem_write(SPRITE_PATTERNS+0x200, smb1spt + 64 * (2 * j + 5), 64);
-  }
+
   /* palette animation (coin / ?-block) */
   if (!(tick & 7)) {
     j = (tick >> 3) & 7;
     vdp_set_palette(7, coin_colors[j]);
   }
+
+  // ---------------------------------------
+  // sprite animation #1
+  {
+    j = tick % 3;
+    vmem_write(SPRITE_PATTERNS+0x100, smb1spt + 64 * (2*j + WALKING_1_R), 64);
+  }
+  // sprite animation #2
+  if (!(tick & 1)) {
+    j = (tick >> 1) % 3;
+    vmem_write(SPRITE_PATTERNS+0x140, smb1spt + 64 * (2*j + WALKING_1_R), 64);
+  }
+  // sprite animation #3
+  if (!(tick & 3)) {
+    j = (tick >> 2) % 3;
+    vmem_write(SPRITE_PATTERNS+0x180, smb1spt + 64 * (2*j + WALKING_1_R), 64);
+  }
+  // sprite animation #4
+  if (!(tick & 7)) {
+    j = (tick >> 3) % 3;
+    vmem_write(SPRITE_PATTERNS+0x1C0, smb1spt + 64 * (2*j + WALKING_1_R), 64);
+  }
 }
 
 static void map_renderer_task(void) {
-  // if (WD <= WX) return;         /* no more map page */
+  // if (map_cols <= map_next) return;         /* no more map page */
 
   // To avoid overflow and to keep invariant,
-  // correct SX (camera position) and WX (next column to be rendered)
-  if (SX >= WD * TILE_WIDTH) {
-    SX -= WD * TILE_WIDTH;
-    WX -= WD;
+  // correct camera position and next column to be rendered
+  if (camera.x.i >= map_cols * TILE_WIDTH) {
+    camera.x.i -= map_cols * TILE_WIDTH;
+    map_next -= map_cols;
   }
 
-  if (SX + 2 * PIXELS_PER_LINE <= TILE_WIDTH * WX) return; /* buffer full */
+  if (camera.x.i + 2 * PIXELS_PER_LINE <= TILE_WIDTH * map_next) {
+    /* buffer full */
+    return;
+  }
 
-  const uint16_t next_column = WX % WD; /* wrap around (loop the stage map) */
-  const uint8_t* p = smb1map + STAGEMAP_PAGE_ROWS * next_column;
-  const uint16_t ix = (TILE_WIDTH * next_column) & 255;
-  const uint16_t pp = (WX & 0x10) << 4; /* page #0 (0) or page #1 (256) */
+  const uint16_t col = map_next % map_cols; // wrap around (loop the stage map)
+  const uint8_t* p = smb1map + STAGEMAP_PAGE_ROWS * col;
+  const uint16_t ix = (TILE_WIDTH * col) & 255;
+  const uint16_t pp = (map_next & 0x10) << 4; // page #0 (0) or page #1 (256)
 
   for (int i = 0; i < STAGEMAP_VISIBLE_ROWS; ++i) {
     const uint16_t iy = TILE_HEIGHT * i + pp;
@@ -253,7 +307,7 @@ static void map_renderer_task(void) {
     const uint16_t y = (tile & 0x70) + 3 * LINES_PER_VRAM_PAGE;
     vdp_cmd_execute_HMMM(x, y, TILE_WIDTH, TILE_HEIGHT, ix, iy);
   }
-  WX++;
+  map_next++;
 }
 
 static void game_core_task(void) {
@@ -263,42 +317,83 @@ static void game_core_task(void) {
   }
   // update camera position
   {
-    SXd += scroll_speed;
-    SX  += (SXd >> 6);
-    SXd &= ((1 << 6) - 1);
+    camera.x.d += camera.speed;
+    camera.x.i += (camera.x.d >> 6);
+    camera.x.d &= ((1 << 6) - 1);
+  }
+  // update mario's state
+  {
+    const f10q6_t speed_hi = f10q6i(10);
+    const f10q6_t speed_lo = f10q6i(6);
+    const uint8_t accel = 16;
+    const uint8_t brake = 24;
+
+    uint8_t FORWARD_KEY;
+    uint8_t BACKWARD_KEY;
+    if (mario_state.facing == FACING_LEFT) {
+      FORWARD_KEY = VK_LEFT;
+      BACKWARD_KEY = VK_RIGHT;
+    } else {
+      FORWARD_KEY = VK_RIGHT;
+      BACKWARD_KEY = VK_LEFT;
+    }
+
+    uint8_t joy = joypad_get_state(1);
+    if (joy & FORWARD_KEY) {
+      if (joy & VK_FIRE_1) {
+        mario_state.speed += 2 * accel;
+        if (speed_hi <= mario_state.speed) {
+          mario_state.speed = speed_hi;
+        }
+      } else if (mario_state.speed < speed_lo) {
+        mario_state.speed += accel;
+      } else {
+        mario_state.speed -= accel;
+      }
+    }
+    if (joy & BACKWARD_KEY) {
+      if (brake < mario_state.speed) {
+        mario_state.speed -= brake;
+      } else {
+        mario_state.speed = 0;
+      }
+    }
+    if (0 < mario_state.speed && !(joy & (VK_LEFT | VK_RIGHT))) {
+      mario_state.speed -= accel;
+    }
+
+    mario_state.input &= 0x30;
+    mario_state.input <<= 2;
+    mario_state.input |= joy & 0x3f;
   }
   // update scroll speed
   {
-    uint8_t joy = joypad_get_state(1);
-    if (joy & VK_RIGHT) {
-      scroll_speed++;
-    }
-    if (joy & VK_LEFT) {
-      if (2 < scroll_speed) {
-        scroll_speed -= 2;
-      } else {
-        scroll_speed = 0;
-      }
-    }
-    if (0 < scroll_speed && !(joy & (VK_LEFT | VK_RIGHT))) {
-      scroll_speed--;
-    }
-    if (scroll_speed > SCROLL_SPEED_MAX) {
-      scroll_speed = SCROLL_SPEED_MAX;
+    camera.speed = mario_state.speed;
+
+    if (SCROLL_SPEED_MAX < camera.speed) {
+      camera.speed = SCROLL_SPEED_MAX;
     }
   }
 }
 
 static void interleave_tasks(void) {
-  while (TIMESLOTS_FOR_CORE_TASK <= timeslots_counter) {
-    timeslots_counter -= TIMESLOTS_FOR_CORE_TASK;
-    if (SX + PIXELS_PER_LINE + 32 > WX * TILE_WIDTH) {
+  // Timeslots ratio of two tasks is
+  // core task : renderer = TILE_WIDTH : CAMERA.SPEED
+  const uint8_t PIX_PER_FRAME = (camera.speed >> 6);
+  const uint8_t TIMESLOTS_1 = TILE_WIDTH;
+  const uint8_t TIMESLOTS_2 = max(PIX_PER_FRAME, 1);
+  const uint8_t margin = 2 * max(TILE_WIDTH, PIX_PER_FRAME);
+  while (TIMESLOTS_1 <= timeslots_counter) {
+    timeslots_counter -= TIMESLOTS_1;
+    while (camera.x.i + PIXELS_PER_LINE + margin > map_next * TILE_WIDTH) {
       map_renderer_task();
     }
   }
-  timeslots_counter += TIMESLOTS_FOR_RENDERER;
-  game_core_task();
+  timeslots_counter += TIMESLOTS_2;
+  // game_core_task();
 }
+
+static volatile __at (0xfc9e) uint16_t JIFFY; // TIME (tick count)
 
 static void play_game(void) {
   setup_stage_map();
@@ -306,21 +401,29 @@ static void play_game(void) {
   vdp_set_sprite_visible(true);
   vdp_set_visible(true);
 
-  WD = smb1map_size / STAGEMAP_PAGE_ROWS; /* number of stage map columns */
-  WX = STAGEMAP_VISIBLE_COLS;   /* next stage map column to be rendered */
-  SX = 0;                       /* integer part of camera position */
-  SXd = 0;                      /* decimal part of camera position */
-  scroll_speed = 0;             /* scroll speed ; 10.6 fixedpoint [pix] */
+  map_cols = smb1map_size / STAGEMAP_PAGE_ROWS; /* number of stage map columns */
+  map_next = STAGEMAP_VISIBLE_COLS;   /* next stage map column to be rendered */
+  camera.x.i = 0;               /* intefer part of camera position */
+  camera.x.d = 0;               /* decimal part of camera position */
+  camera.speed = 0;             /* scroll speed ; 10.6 fixedpoint [pix] */
   timeslots_counter = 0;     /* accumulator for cooperative task interleaving */
 
-  for (tick = 0; ; ++tick) {
+  mario_state.input = 0;
+  mario_state.speed = 0;
+  mario_state.facing = FACING_RIGHT;
+
+  for (JIFFY = tick = 0; ;) {
+    ++tick;
     // wait for VSYNC interrupt and interrupt handler finished
     await_interrupt();
     // ---- sound / visual output task ----
-    vdp_set_hscroll(SX & (2 * PIXELS_PER_LINE - 1));
+    vdp_set_hscroll(camera.x.i & (2 * PIXELS_PER_LINE - 1));
     animation_task();
     // ---- cooperative task interleaving ----
     interleave_tasks();
+    if (tick & 1) {
+      game_core_task();
+    }
   }
 }
 
