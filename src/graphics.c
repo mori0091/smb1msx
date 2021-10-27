510c1a5afdf2f8aa8ae0e16e196399000631b391
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-10-27 13:18:03 +0900

    Fixed a problem with VRAM initialization.
    Unexpected sprites were sometimes displayed. This is due to the fact that in
    rare cases, the timing of the access to VRAM may not be correct. This has been
    fixed by writing the same data to VRAM several times.

--- src/graphics.c
+++ src/graphics.c
@@ -31,13 +31,18 @@ void graphics_clear_vram(void) {
   /* Fill the pattern name table */
   vdp_cmd_execute_HMMV(0, 0, PIXELS_PER_LINE, 2 * LINES_PER_VRAM_PAGE, 0x00);
 
-  /* Clear sprite pattern table and sprite color table */
-  vmem_memset(SPRITE_PATTERNS, 0, SIZE_OF_SPRITE_PATTERNS);
-  vmem_memset(SPRITE_COLORS, 0, SIZE_OF_SPRITE_COLORS);
+  // In rare cases, access to VRAM may fail or may be omitted depending on the
+  // timing of the access. Therefore, repeats the VRAM initialization several
+  // times.
+  for (int j = 0; j < 2; ++j) {
+    /* Clear sprite pattern table and sprite color table */
+    vmem_memset(SPRITE_PATTERNS, 0, SIZE_OF_SPRITE_PATTERNS);
+    vmem_memset(SPRITE_COLORS, 0, SIZE_OF_SPRITE_COLORS);
 
-  /* Clear sprites */
-  struct sprite s = { .y = 217 };
-  for (int i = 0; i < 32; ++i) {
-    vmem_write(SPRITES + sizeof(struct sprite) * i, &s, sizeof(struct sprite));
+    /* Clear sprites */
+    struct sprite s = { .y = 217 };
+    for (int i = 0; i < 32; ++i) {
+      vmem_write(SPRITES + sizeof(struct sprite) * i, &s, sizeof(struct sprite));
+    }
   }
 }
