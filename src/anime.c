a5ab442f674ea4d7c0752d17147dbbea38381b96
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-10-28 23:51:15 +0900

    Removed the debug purpose sprite animation.

--- src/anime.c
+++ src/anime.c
@@ -3,15 +3,15 @@
 #include "smb1.h"
 
 void anime_show_sprites(void) {
-  // Show 16 sprite characters for demo
-  struct sprite s = {0};
-  for (int i = 0; i < 32; ++i) {
-    int x =  8 + 16 * (i / 2 % 4);
-    int y = -1 + 16 * (i / 8);
-    sprite_set_xy(&s, x, y);
-    s.pat = 4 * i;
-    vmem_set_sprite(SPRITES, i, &s);
-  }
+  // // Show 16 sprite characters for demo
+  // struct sprite s = {0};
+  // for (int i = 0; i < 32; ++i) {
+  //   int x =  8 + 16 * (i / 2 % 4);
+  //   int y = -1 + 16 * (i / 8);
+  //   sprite_set_xy(&s, x, y);
+  //   s.pat = 4 * i;
+  //   vmem_set_sprite(SPRITES, i, &s);
+  // }
 }
 
 void anime_clear_sprites(void) {
@@ -28,8 +28,6 @@ static void demo_animate(void);
 void anime_update(void) {
   mario_animate();
   coin_palette_animate();
-  // ---------------------------------------
-  demo_animate();               /* for debug purpose */
 }
 
 
@@ -45,29 +43,4 @@ static void coin_palette_animate(void) {
     const uint8_t j = (tick >> 3) & 7;
     vdp_set_palette(7, coin_colors[j]);
   }
-}
-
-static void demo_animate(void) {
-  const uint8_t p = WALKING_1 + mario_state.facing;
-  uint8_t j;
-  // sprite animation #1 (every 1 frame ; 60fps @ 60Hz)
-  {
-    j = tick % 3;
-    vmem_write(SPRITE_PATTERNS+0x100, smb1spt + 64 * (2*j + p), 64);
-  }
-  // // sprite animation #2 (every 2 frames ; 30fps @ 60Hz)
-  // if (!(tick & 1 ^ 1)) {
-  //   j = (tick >> 1) % 3;
-  //   vmem_write(SPRITE_PATTERNS+0x140, smb1spt + 64 * (2*j + p), 64);
-  // }
-  // // sprite animation #3 (every 4 frames ; 15fps @ 60Hz)
-  // if (!(tick & 3 ^ 2)) {
-  //   j = (tick >> 2) % 3;
-  //   vmem_write(SPRITE_PATTERNS+0x180, smb1spt + 64 * (2*j + p), 64);
-  // }
-  // // sprite animation #4 (every 8 frames ; 7.5fps @ 60Hz)
-  // if (!(tick & 7 ^ 4)) {
-  //   j = (tick >> 3) % 3;
-  //   vmem_write(SPRITE_PATTERNS+0x1C0, smb1spt + 64 * (2*j + p), 64);
-  // }
 }
