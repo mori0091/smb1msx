40162fd190aadfe8cdcbc1fc41c1d1f97614cf80
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-11-25 03:31:03 +0900

    Fixed `interrupted` flag that was set/reset in the wrong order.

--- src/hsync.c
+++ src/hsync.c
@@ -8,16 +8,16 @@ volatile uint16_t scroll_x;
 
 static volatile bool interrupted;
 
-static void vsync_handler(void) {
+inline void vsync_handler(void) {
   // vdp_set_hscroll(0);
   {
     VDP_SET_CONTROL_REGISTER(26, 0);
     VDP_SET_CONTROL_REGISTER(27, 0);
   }
+  interrupted = true;
 }
 
 inline void vsync_handler_epilogue(void) {
-  interrupted = true;
   vsync_busy = false;
   JIFFY++;
   __asm__("ei");
@@ -38,6 +38,7 @@ inline void hsync_handler(void) {
       RG26SA++;
     }
   }
+  interrupted = false;
 }
 
 static void interrupt_handler(void) {
@@ -45,7 +46,6 @@ static void interrupt_handler(void) {
   VDP_SET_STATUS_REGISTER_POINTER(1);
   if (VDP_GET_STATUS_REGISTER_VALUE() & 1) {
     hsync_handler();
-    interrupted = false;
     // Don't forget reset the status register pointer to 0
     VDP_SET_STATUS_REGISTER_POINTER(0);
     // Checking if (delayed) VSYNC is caught at the same time
