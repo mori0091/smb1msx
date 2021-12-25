b4816ebe2db89f1673c598e4df8c8b783028e0e2
mori0091 <mori-d@qc4.so-net.ne.jp>
2021-12-26 08:47:50 +0900

    Improved the rendering performance of countdown timer values.

--- src/countdown.c
+++ src/countdown.c
@@ -22,6 +22,40 @@ void countdown_timer_print(void) {
   text_putc('0' + (t & 15));
 }
 
+inline void countdown_timer_print_diff(void) {
+  if (!visible) {
+    return;
+  }
+  set_foreground_color(14);
+
+  uBCD16_t t = remaining;
+  uint8_t d;
+
+  locate(228, 8);
+  d = t & 15;
+  text_putc('0' + d);
+  if (d < 9) return;
+  t >>= 4;
+
+  locate(220, 8);
+  if (!t) {
+    text_putc(' ');
+    return;
+  }
+  d = t & 15;
+  text_putc('0' + d);
+  if (d < 9) return;
+  t >>= 4;
+
+  locate(212, 8);
+  if (!t) {
+    text_putc(' ');
+    return;
+  }
+  d = t & 15;
+  text_putc('0' + d);
+}
+
 void countdown_timer_set(uBCD16_t t) {
   remaining = t;
   elapsed = 0;
@@ -46,7 +80,7 @@ void countdown_timer_update(void) {
       __asm__("ld (_remaining + 1), a");
       __asm__("0001$:");
     }
-    countdown_timer_print();
+    countdown_timer_print_diff();
     if (remaining == 0x099) {
       sound_effect(&se_hurryup);
       sound_set_speed((uint8_t)(1.5 * SOUND_SPEED_1X));
