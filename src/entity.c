17add37704ed6a36ed36c9c833394de637d6a6c0
mori0091 <mori-d@qc4.so-net.ne.jp>
2022-02-05 02:42:38 +0900

    Added mushroom rise animation.

--- src/entity.c
+++ src/entity.c
@@ -57,41 +57,58 @@ static metasprite_t block_metasprite = {
 
 static entity_t item_entity;
 static uint8_t item0;
+static uint8_t item_tick;
+static uint16_t item_x0, item_y0;
 
 static entity_t block_entity;
-static uint8_t row0, col0;
 static uint8_t tile0;
+static uint8_t block_tick;
+static uint8_t row0, col0;
 
 static uint8_t mushroom_controller(void) {
-  if (item_entity.collision & COLLISION_LEFT) {
-    item_entity.vel.x = MUSHROOM_VX;
-  }
   if (item_entity.collision & COLLISION_RIGHT) {
     item_entity.vel.x = -MUSHROOM_VX;
+  }
+  if (item_entity.collision & COLLISION_LEFT) {
+    item_entity.vel.x = MUSHROOM_VX;
   }
   return (item_entity.vel.x < 0 ? VK_LEFT : VK_RIGHT);
 }
 
-static void mushroom_post_step(entity_t * e) {
+void default_post_step(entity_t * e) {
   uint16_t x0 = camera_get_x();
-  if ((e->pos.x.i < x0)       ||
-      (x0 + 256 < e->pos.x.i) ||
+  if ((e->pos.x.i <= x0 - 64)        ||
+      (x0 + 256+64-16 <= e->pos.x.i) ||
       (212 <= e->pos.y.i)) {
     entity_remove(e);
     return;
   }
-  if (e->vel.x < -MUSHROOM_VX) {
-    e->vel.x = -MUSHROOM_VX;
+}
+
+void mushroom_post_step(entity_t * e) {
+  if (item_tick--) {
+    e->pos.x.i = item_x0;
+    e->pos.y.i = item_y0 + item_tick;
   }
-  if (e->vel.x > +MUSHROOM_VX) {
-    e->vel.x = +MUSHROOM_VX;
+  else {
+    e->pos.x.i = item_x0;
+    e->pos.x.d = 0;
+    e->pos.y.i = item_y0;
+    e->pos.y.d = 0;
+    e->vel.x = MUSHROOM_VX;
+    e->vel.y = 0;
+    e->acc.x = 0;
+    e->acc.y = MUSHROOM_AY;
+    e->collision = 0;
+    entity_set_controller(e, mushroom_controller);
+    entity_set_post_step(e, default_post_step);
   }
 }
 
 static void mushroom_entity_new(uint8_t row, uint8_t col) {
   entity_remove(&item_entity);
 
-  entity_set_controller(&item_entity, mushroom_controller);
+  entity_set_controller(&item_entity, no_controller);
   entity_set_post_step(&item_entity, mushroom_post_step);
   entity_set_metasprite(&item_entity, &mushroom_metasprite);
   // entity_set_sprite_palette(&item_entity, MUSHROOM_PALETTE);
@@ -100,18 +117,21 @@ static void mushroom_entity_new(uint8_t row, uint8_t col) {
                             ? MUSHROOM_PALETTE
                             : GREEN_MUSHROOM_PALETTE);
   item_entity.plane = PLANE_ITEMS;
-
   item_entity.input = 0;
-  item_entity.pos.x.i = col * TILE_WIDTH;
-  item_entity.pos.x.d = 0;
-  item_entity.pos.y.i = row * TILE_HEIGHT;
-  item_entity.pos.y.d = 0;
-  item_entity.vel.x = 0;
-  item_entity.vel.y = 0;
-  item_entity.acc.x = 0;
-  item_entity.acc.y = MUSHROOM_AY;
-
+  item_x0 = col * TILE_WIDTH;
+  item_y0 = row * TILE_HEIGHT;
+  item_tick = 12;
   entity_add(&item_entity);
+}
+
+static void block_post_step2(entity_t * e) {
+  if (block_tick--) {
+    e->pos.x.i = col0 * TILE_WIDTH;
+    e->pos.y.i = row0 * TILE_HEIGHT;
+  }
+  else {
+    entity_remove(e);
+  }
 }
 
 static void block_post_step(entity_t * e) {
@@ -119,7 +139,6 @@ static void block_post_step(entity_t * e) {
     return;
   }
   put_tile(row0, col0, tile0);
-  entity_remove(e);
   switch (item0) {
   case ITEM_NONE:
     break;
@@ -128,12 +147,15 @@ static void block_post_step(entity_t * e) {
   case ITEM_MUSHROOM:
   case ITEM_1UP_MUSHROOM:
     mushroom_entity_new(row0 - 1, col0);
-    break;
+    block_tick = 12;
+    entity_set_post_step(e, block_post_step2);
+    return;
   case ITEM_FIREFLOWER:
   case ITEM_STARMAN:
   default:
     break;
   }
+  entity_remove(e);
 }
 
 static void block_entity_new(uint8_t row, uint8_t col, uint8_t tile) {
