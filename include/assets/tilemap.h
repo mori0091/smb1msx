542eada3265cee4aefa7ae36eab7f10c84df3ecd
mori0091 <mori-d@qc4.so-net.ne.jp>
2022-05-25 21:57:16 +0900

    feat: add `tilemap` module

---
+++ include/assets/tilemap.h
@@ -0,0 +1,206 @@
+// -*- coding: utf-8-unix -*-
+
+#pragma once
+
+#ifndef TILEMAP_H_
+#define TILEMAP_H_
+
+#include <stdint.h>
+
+/**
+ * Enumerations of matrix transformation.
+ *
+ * \sa tilemap_copy_to_vmem()
+ * \sa tilemap_copy_to_vmem_hflip()
+ * \sa tilemap_copy_to_vmem_vflip()
+ * \sa tilemap_copy_to_vmem_4()
+ * \sa tilemap_copy_to_vmem_4_hflip()
+ * \sa tilemap_copy_to_vmem_4_vflip()
+ */
+enum {
+  IDENTITY        = 0,
+  FLIP_HORIZONTAL = (1 << 7),
+  FLIP_VERTICAL   = (1 << 6),
+  TRANSPOSE       = (1 << 5),
+  ROTATE_90       = (FLIP_HORIZONTAL | TRANSPOSE),
+  ROTATE_180      = (FLIP_VERTICAL | FLIP_HORIZONTAL),
+  ROTATE_270      = (FLIP_VERTICAL | TRANSPOSE),
+  ROTATE_90_CCW   = ROTATE_270,
+  ROTATE_180_CCW  = ROTATE_180,
+  ROTATE_270_CCW  = ROTATE_90,
+};
+
+/**
+ * A 256x8 matrix of uint8_t type.
+ */
+typedef uint8_t u8_256x8[256][8];
+
+/**
+ * Copy 1 bpp tile pattern of 8x8 pixels to VRAM according to the given tilemap.
+ *
+ * This function, according to the given `tilemap`, reads 1bpp tile pattern of
+ * 8x8 pixels from the given `tileset` and applies some matrix transformation,
+ * and then write it to VRAM.
+ *
+ * A word `tileset` here means a series of 1 bpp tile pattern of 8x8 pixels,
+ * that is represented as 256x8 matrix of uint8_t type. So its size shall be
+ * 2KiB (2048 bytes = 256 tiles * 8 bytes/tile).
+ *
+ * This function can takes one or more `tileset`. If two or more tilesets are
+ * passed (i.e. `m > 1`), they are treated as multi-layered tilesets.
+ *
+ * A word `tilemap` here means a meta-data to make a complete tiled image by
+ * composing 8x8 pixels tiles picked up from `tileset`.
+ *
+ * `tilemap` is represeted as a list of `(idx, attr)` pairs.
+ * - The 1st byte `idx` shall be the index number of a tile in `tileset`. (0..255)
+ * - The 2nd byte `attr` shall be 0 or OR-ed set of the following matrix transformation:
+ *   - FLIP_HORIZONTAL
+ *   - FLIP_VERTICAL
+ *   - TRANSPOSE
+ *
+ * When two or more matrix transformation are specified, they are applied as
+ * following order:
+ * ~~~
+ * TRANSPOSE >> FLIP_VERTICAL >> FLIP_HORIZONTAL
+ * ~~~
+ *
+ * For example, when `n = 4` and `m = 2`, the resulting VRAM memory map will be
+ * as follows:
+ * ~~~
+ * +0x0000 +-------------------------------------+
+ *         | pattern #0 / layer #0 / line #0..#7 | <- f0(tileset[0][idx0])
+ * +0x0008 +-------------------------------------+
+ *         | pattern #0 / layer #1 / line #0..#7 | <- f0(tileset[1][idx0])
+ * +0x0010 +-------------------------------------+
+ *         | pattern #1 / layer #0 / line #0..#7 | <- f1(tileset[0][idx1])
+ * +0x0018 +-------------------------------------+
+ *         | pattern #1 / layer #1 / line #0..#7 | <- f1(tileset[1][idx1])
+ * +0x0020 +-------------------------------------+
+ *         | pattern #2 / layer #0 / line #0..#7 | <- f2(tileset[0][idx2])
+ * +0x0028 +-------------------------------------+
+ *         | pattern #2 / layer #1 / line #0..#7 | <- f2(tileset[1][idx2])
+ * +0x0030 +-------------------------------------+
+ *         | pattern #3 / layer #0 / line #0..#7 | <- f3(tileset[0][idx3])
+ * +0x0038 +-------------------------------------+
+ *         | pattern #3 / layer #1 / line #0..#7 | <- f3(tileset[1][idx3])
+ * +0x0040 +-------------------------------------+
+ *
+ * where
+ * - idx<i> is an index number represented by tilemap[i][0]
+ * - f<i>() is a matrix transformation represented by tilemap[i][1]
+ * ~~~
+ *
+ * \param n        the number of elements of tilemap
+ * \param tilemap  n-element list of (idx, attr) tupples
+ * \param m        the number of leyars / the number of tilesets
+ * \param tileset  a list of pointers to tileset. (one tileset per one layer)
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ */
+void tilemap_copy_to_vmem(uint8_t n, const uint8_t tilemap[/* n */][2],
+                          uint8_t m, const u8_256x8 * tileset[/* m */]);
+
+/**
+ * Same as tilemap_copy_to_vmem() but resulting tiles are flipped horizonally.
+ *
+ * \param n        the number of elements of tilemap
+ * \param tilemap  n-element list of (idx, attr) tupples
+ * \param m        the number of leyars / the number of tilesets
+ * \param tileset  a list of pointers to tileset. (one tileset per one layer)
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ */
+void tilemap_copy_to_vmem_hflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                uint8_t m, const u8_256x8 * tileset[/* m */]);
+
+/**
+ * Same as tilemap_copy_to_vmem() but resulting tiles are flipped vertically.
+ *
+ * \param n        the number of elements of tilemap
+ * \param tilemap  n-element list of (idx, attr) tupples
+ * \param m        the number of leyars / the number of tilesets
+ * \param tileset  a list of pointers to tileset. (one tileset per one layer)
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ */
+void tilemap_copy_to_vmem_vflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                uint8_t m, const u8_256x8 * tileset[/* m */]);
+
+/**
+ * Same as tilemap_copy_to_vmem() but tilemap is processed for each 4 elements.
+ *
+ * This function is same as tilemap_copy_to_vmem() except for order of the
+ * resulting VRAM memory map.
+ *
+ * In contrast to tilemap_copy_to_vmem(), this function processes `tilemap` for
+ * each 4 elements. Therefore the resulting VRAM image of this function may be
+ * used as "(multi-layer) sprite pattern generator table" consists of 16x16
+ * pixels sprite patterns.
+ *
+ * For example, when `n = 4` and `m = 2`, the resulting VRAM memory map
+ * will be as follows:
+ * ~~~
+ * +0x0000 +-------------------------------------+
+ *         | pattern #0 / layer #0 / line #0..#7 | <- f0(tileset[0][idx0]) ; top-left
+ * +0x0008 +-------------------------------------+
+ *         | pattern #1 / layer #0 / line #0..#7 | <- f1(tileset[0][idx1]) ; bottom-left
+ * +0x0010 +-------------------------------------+
+ *         | pattern #2 / layer #0 / line #0..#7 | <- f2(tileset[0][idx2]) ; top-right
+ * +0x0018 +-------------------------------------+
+ *         | pattern #3 / layer #0 / line #0..#7 | <- f3(tileset[0][idx3]) ; bottom-right
+ * +0x0020 +-------------------------------------+
+ *         | pattern #0 / layer #1 / line #0..#7 | <- f0(tileset[1][idx0]) ; top-left
+ * +0x0028 +-------------------------------------+
+ *         | pattern #1 / layer #1 / line #0..#7 | <- f1(tileset[1][idx1]) ; bottom-left
+ * +0x0030 +-------------------------------------+
+ *         | pattern #2 / layer #1 / line #0..#7 | <- f2(tileset[1][idx2]) ; top-right
+ * +0x0038 +-------------------------------------+
+ *         | pattern #3 / layer #1 / line #0..#7 | <- f3(tileset[1][idx3]) ; bottom-right
+ * +0x0040 +-------------------------------------+
+ *
+ * where
+ * - idx<i> is an index number represented by tilemap[i][0]
+ * - f<i>() is a matrix transformation represented by tilemap[i][1]
+ * ~~~
+ *
+ * \param n        the number of elements of tilemap (multiple of 4)
+ * \param tilemap  n-element list of (idx, attr) tupples
+ * \param m        the number of leyars / the number of tilesets
+ * \param tileset  a list of pointers to tileset. (one tileset per one layer)
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ *
+ * \note If `m` is 1, this function produces exactly the same result as
+ * tilemap_copy_to_vmem().
+ */
+void tilemap_copy_to_vmem_4(uint8_t n, const uint8_t tilemap[/* n */][2],
+                            uint8_t m, const u8_256x8 * tileset[/* m */]);
+
+/**
+ * Same as tilemap_copy_to_vmem_4() but resulting tiles are flipped horizonally.
+ *
+ * \param n        the number of elements of tilemap (multiple of 4)
+ * \param tilemap  n-element list of (idx, attr) tupples
+ * \param m        the number of leyars / the number of tilesets
+ * \param tileset  a list of pointers to tileset. (one tileset per one layer)
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ */
+void tilemap_copy_to_vmem_4_hflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                  uint8_t m, const u8_256x8 * tileset[/* m */]);
+
+/**
+ * Same as tilemap_copy_to_vmem_4() but resulting tiles are flipped vertically.
+ *
+ * \param n        the number of elements of tilemap (multiple of 4)
+ * \param tilemap  n-element list of (idx, attr) tupples
+ * \param m        the number of leyars / the number of tilesets
+ * \param tileset  a list of pointers to tileset. (one tileset per one layer)
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ */
+void tilemap_copy_to_vmem_4_vflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                  uint8_t m, const u8_256x8 * tileset[/* m */]);
+
+#endif // TILEMAP_H_
---
+++ src/bank1/assets/sprite/tilemap.c
@@ -0,0 +1,195 @@
+// -*- coding: utf-8-unix -*-
+
+#pragma codeseg BANK1
+
+#include <stdint.h>
+#include <string.h>
+
+#include <vdp.h>
+#include <vmem.h>
+
+#include "macros.h"
+
+#include "assets/tilemap.h"
+
+static uint8_t h_flip_8bit(uint8_t x) {
+  x = ((x << 1) & 0xaa) | ((x >> 1) & 0x55);
+  x = ((x << 2) & 0xcc) | ((x >> 2) & 0x33);
+  x = ((x << 4) & 0xf0) | ((x >> 4) & 0x0f);
+  return x;
+}
+
+static void h_flip_8x8_1bpp(const uint8_t src[8], uint8_t dst[8]) {
+  uint8_t n = 8;
+  while (n--) {
+    *dst++ = h_flip_8bit(*src++);
+  }
+}
+
+static uint8_t tmp[8];
+
+static void v_flip_8x8_1bpp(const uint8_t src[8], uint8_t dst[8]) {
+  uint8_t * q = tmp + 7;
+  uint8_t n = 8;
+  while (n--) {
+    *q-- = *src++;
+  }
+  memcpy(dst, tmp, 8);
+}
+
+static void transpose_8x8_1bpp(const uint8_t src[8], uint8_t dst[8]) {
+  for (uint8_t i = 0; i < 8; ++i) {
+    uint8_t x = src[i];
+    uint8_t * q = tmp;
+    for (uint8_t j = 0; j < 8; ++j) {
+      *q <<= 1;
+      if (x & 0x80) {
+        *q |= 1;
+      }
+      q++;
+      x <<= 1;
+    }
+  }
+  memcpy(dst, tmp, 8);
+}
+
+/**
+ * Copy 1 bpp tile pattern of 8x8 pixels to VRAM with or without matrix transformation.
+ *
+ * `attr` shall be 0 or OR-ed set of the following matrix transformation:
+ * - FLIP_HORIZONTAL
+ * - FLIP_VERTICAL
+ * - TRANSPOSE
+ *
+ * When two or more matrix transformation is applied, they are applied as following order:
+ * ~~~
+ * TRANSPOSE >> FLIP_VERTICAL >> FLIP_HORIZONTAL
+ * ~~~
+ *
+ * \param attr  attribute set of matrix transformation
+ * \param tile  1bpp tile pattern of 8x8 pixels
+ *
+ * \note vmem_set_write_address() is required before call to this function.
+ */
+static void copy_tile_to_vmem(uint8_t attr, const uint8_t tile[8]) {
+  static uint8_t buf[8];
+  if (attr & TRANSPOSE) {
+    transpose_8x8_1bpp(tile, buf);
+  }
+  else {
+    memcpy(buf, tile, 8);
+  }
+  if (attr & FLIP_VERTICAL) {
+    v_flip_8x8_1bpp(buf, buf);
+  }
+  if (attr & FLIP_HORIZONTAL) {
+    h_flip_8x8_1bpp(buf, buf);
+  }
+  uint8_t * p = buf;
+  uint8_t n = 8;
+  while (n--) {
+    vmem_set(*p++);
+  }
+}
+
+void tilemap_copy_to_vmem(uint8_t n, const uint8_t tilemap[/* n */][2],
+                          uint8_t m, const u8_256x8 * tileset[/* m */]) {
+  const uint8_t * p = (const uint8_t *)tilemap;
+  while (n--) {
+    const uint8_t idx = *p++;
+    const uint8_t attr = *p++;
+    for (uint8_t i = 0; i < m; ++i) {
+      copy_tile_to_vmem(attr, (*tileset[i])[idx]);
+    }
+  }
+}
+
+void tilemap_copy_to_vmem_hflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                uint8_t m, const u8_256x8 * tileset[/* m */]) {
+  const uint8_t * p = (const uint8_t *)tilemap;
+  while (n--) {
+    const uint8_t idx = *p++;
+    const uint8_t attr = *p++ ^ FLIP_HORIZONTAL;
+    for (uint8_t i = 0; i < m; ++i) {
+      copy_tile_to_vmem(attr, (*tileset[i])[idx]);
+    }
+  }
+}
+
+void tilemap_copy_to_vmem_vflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                uint8_t m, const u8_256x8 * tileset[/* m */]) {
+  const uint8_t * p = (const uint8_t *)tilemap;
+  while (n--) {
+    const uint8_t idx = *p++;
+    const uint8_t attr = *p++ ^ FLIP_VERTICAL;
+    for (uint8_t i = 0; i < m; ++i) {
+      copy_tile_to_vmem(attr, (*tileset[i])[idx]);
+    }
+  }
+}
+
+void tilemap_copy_to_vmem_4(uint8_t n, const uint8_t tilemap[/* n */][2],
+                            uint8_t m, const u8_256x8 * tileset[/* m */]) {
+  const uint8_t * p = (const uint8_t *)tilemap;
+  for (uint8_t i = 0; i < n; i += 4) {
+    const uint8_t idx0 = *p++;
+    const uint8_t attr0 = *p++;
+    const uint8_t idx1 = *p++;
+    const uint8_t attr1 = *p++;
+    const uint8_t idx2 = *p++;
+    const uint8_t attr2 = *p++;
+    const uint8_t idx3 = *p++;
+    const uint8_t attr3 = *p++;
+    for (uint8_t j = 0; j < m; ++j) {
+      const u8_256x8 * ts = tileset[j];
+      copy_tile_to_vmem(attr0, (*ts)[idx0]);
+      copy_tile_to_vmem(attr1, (*ts)[idx1]);
+      copy_tile_to_vmem(attr2, (*ts)[idx2]);
+      copy_tile_to_vmem(attr3, (*ts)[idx3]);
+    }
+  }
+}
+
+void tilemap_copy_to_vmem_4_hflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                  uint8_t m, const u8_256x8 * tileset[/* m */]) {
+  const uint8_t * p = (const uint8_t *)tilemap;
+  for (uint8_t i = 0; i < n; i += 4) {
+    const uint8_t idx0 = *p++;
+    const uint8_t attr0 = *p++ ^ FLIP_HORIZONTAL;
+    const uint8_t idx1 = *p++;
+    const uint8_t attr1 = *p++ ^ FLIP_HORIZONTAL;
+    const uint8_t idx2 = *p++;
+    const uint8_t attr2 = *p++ ^ FLIP_HORIZONTAL;
+    const uint8_t idx3 = *p++;
+    const uint8_t attr3 = *p++ ^ FLIP_HORIZONTAL;
+    for (uint8_t j = 0; j < m; ++j) {
+      const u8_256x8 * ts = tileset[j];
+      copy_tile_to_vmem(attr2, (*ts)[idx2]);
+      copy_tile_to_vmem(attr3, (*ts)[idx3]);
+      copy_tile_to_vmem(attr0, (*ts)[idx0]);
+      copy_tile_to_vmem(attr1, (*ts)[idx1]);
+    }
+  }
+}
+
+void tilemap_copy_to_vmem_4_vflip(uint8_t n, const uint8_t tilemap[/* n */][2],
+                                  uint8_t m, const u8_256x8 * tileset[/* m */]) {
+  const uint8_t * p = (const uint8_t *)tilemap;
+  for (uint8_t i = 0; i < n; i += 4) {
+    const uint8_t idx0 = *p++;
+    const uint8_t attr0 = *p++ ^ FLIP_VERTICAL;
+    const uint8_t idx1 = *p++;
+    const uint8_t attr1 = *p++ ^ FLIP_VERTICAL;
+    const uint8_t idx2 = *p++;
+    const uint8_t attr2 = *p++ ^ FLIP_VERTICAL;
+    const uint8_t idx3 = *p++;
+    const uint8_t attr3 = *p++ ^ FLIP_VERTICAL;
+    for (uint8_t j = 0; j < m; ++j) {
+      const u8_256x8 * ts = tileset[j];
+      copy_tile_to_vmem(attr1, (*ts)[idx1]);
+      copy_tile_to_vmem(attr0, (*ts)[idx0]);
+      copy_tile_to_vmem(attr3, (*ts)[idx3]);
+      copy_tile_to_vmem(attr2, (*ts)[idx2]);
+    }
+  }
+}
